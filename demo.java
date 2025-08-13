cc0d8e2a35b37f22eca7653261154111504cea37
unknown <alitta.dennis@gmail.com>
2025-08-13 16:58:11 +0530

    first

---
+++ demo.java
@@ -0,0 +1,56 @@
+// class calculator
+// {   
+//     public int add(int a, int b)
+//     {
+//         int r= a+b;
+//         return r;
+//     }
+// }
+
+
+class demo {
+    public static void main(String []args) {
+        int num1= 10;
+        int num2= 20;
+        int num3= 40;
+        if(num1>num2 && num1>num3)
+            System.out.println(num1);
+        else if(num2>num1 && num2>num3)
+            System.out.println(num2);
+        else 
+            System.out.println(num3);
+
+        int d=4;
+
+        switch(d)
+        {
+            case 1: System.out.println("Sunday");break;
+            case 2: System.out.println("Monday");break;
+            case 3: System.out.println("Tuesday");break;
+            case 4: System.out.println("Wednesday");break;
+            case 5: System.out.println("Thursday");break;
+            case 6: System.out.println("Friday");break;
+            case 7: System.out.println("Saturday");break;
+            default: System.out.println("Not valid");
+        }
+        for(int i=1; i<=5; i++)
+        {
+            System.out.println(i);
+        }
+
+        int j=6;
+        while(j<=10)
+        {
+            System.out.println(j);
+            j++;
+        }
+
+        do{
+            System.out.println(j);
+        }while(j<10);
+        
+        // calculator calc= new calculator();
+        // int result = calc.add(num1,num2);
+        // System.out.println(result);
+    }
+}
